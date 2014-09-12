aeac66b5c4ef8dc455837cdbcca207e0b2e1ff55
Blanchet <me@nblanchet.com>
2014-09-12 08:05:47 -0400

    10 solutions

---
+++ 10_Forms/solutions/shirts_app/app/controllers/shirts_controller.rb
@@ -0,0 +1,47 @@
+class ShirtsController < ApplicationController
+  before_action :load_shirt, only: [ :edit, :update, :show, :destroy ]
+
+  def index
+    @shirts = Shirt.search_for(params[:q])
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @shirt = Shirt.new
+  end
+
+  def update
+    @shirt.update safe_shirt_params
+    redirect_to @shirt
+  end
+
+  def create
+    @shirt = Shirt.new(safe_shirt_params)
+    if @shirt.save # valid!
+      redirect_to @shirt  # shirt_path(@shirt)
+    else # invalid!
+      render 'new'
+    end
+  end
+
+  def destroy
+    @shirt.delete
+    redirect_to shirts_path
+  end
+
+  private
+
+  def safe_shirt_params
+    params.require('shirt').permit(:name, :description, :image)
+  end
+
+  def load_shirt
+    @shirt = Shirt.find(params[:id])
+  end
+
+end
